ef3727f13bad193e179d74a97b8bee27180b554f
saidou diallo <saidou224dev@hotmail.com>
2021-07-26 14:51:35 +0000

    ajout de la function fléchée

--- navigations/AppNavigation.js
+++ navigations/AppNavigation.js
@@ -3,7 +3,7 @@ import React from 'react';
 import { createStackNavigator } from '@react-navigation/stack';
 
 const Stack=createStackNavigator();
-function AppNavigation(props) {
+AppNavigation(props)=>{
     return<Stack.Navigator>
         <Stack.Screen
             name="Intro"
